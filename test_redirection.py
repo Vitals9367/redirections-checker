34126ab4df208a1a5010df173908de853ea84e55
Vitals9367 <vitalijus.alsauskas@gmail.com>
2023-03-16 20:55:27 +0200

    Initial commit

---
+++ test_redirection.py
@@ -0,0 +1,18 @@
+# Script compares if both servers have same path redirections 
+import requests
+
+HELFI_HOST="https://www.hel.fi"
+PROXY_HOST="https://seo-redirector-dev.agw.arodevtest.hel.fi"
+
+file1 = open('paths.txt', 'r')
+PATHS = file1.readlines()
+  
+for path in PATHS:
+    path.strip()
+    
+    helfi_request = requests.get(HELFI_HOST+path)
+    proxy_request = requests.get(PROXY_HOST+path)
+    
+    if helfi_request != proxy_request:
+        text = f"""{'-'*40} \nRedirection mismatch \nSOURCE = {path}\nHelfi redirected to = {helfi_request.url}\nproxy redirected to = {proxy_request.url}\n{'-'*40}\n"""
+        print(text)
