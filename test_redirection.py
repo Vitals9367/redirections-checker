864cf6f11a41d23298739664416d4da0cee50fdf
Vitals9367 <vitalijus.alsauskas@gmail.com>
2023-03-17 19:23:05 +0200

    Change to async

--- test_redirection.py
+++ test_redirection.py
@@ -1,18 +1,53 @@
-# Script compares if both servers have same path redirections 
-import requests
+# Script compares if both servers have same path redirections
+import asyncio
+import aiohttp
+import argparse
 
-HELFI_HOST="https://www.hel.fi"
-PROXY_HOST="https://seo-redirector-dev.agw.arodevtest.hel.fi"
+parser = argparse.ArgumentParser(description="Script sends request to both HTTP servers with the same path and compares if it returns same URL",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-o", "--output", help="Log output file")
+parser.add_argument("pathsfile", help="File containing paths, paths have to be seperated by new line and must start with /")
+parser.add_argument("server1", help="sirst Server")
+parser.add_argument("server2", help="second Server")
+args = parser.parse_args()
+config = vars(args)
 
-file1 = open('paths.txt', 'r')
-PATHS = file1.readlines()
-  
-for path in PATHS:
-    path.strip()
+SERVER1_HOST=config["server1"]
+SERVER2_HOST=config["server2"]
+PATHS_FILE=config["pathsfile"]
+OUTPUT=config["output"]
+
+def read_paths(file):
+    paths_file = open(file, 'r')
+    paths = paths_file.readlines()
+    paths_file.close()
+    return paths
+
+def log(text):
+    print(f'{text}\n')
+    if(OUTPUT):
+        log_file = open(OUTPUT, 'r')
+        log_file.writelines(f'{text}\n')
+        log_file.close()
+
+async def send_request(session, url):
+    async with session.get(url) as response:
+        return response.url
+
+async def compare_urls(paths):
+    for path in paths:
+        async with aiohttp.ClientSession() as session:
+            server1_response = await send_request(session, SERVER1_HOST+path)
+            server2_response = await send_request(session, SERVER2_HOST+path)
+            
+            if(server1_response != server2_response):
+                log(server1_response)
+            
+
+def main():
+    paths = read_paths(PATHS_FILE)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(compare_urls(paths))
     
-    helfi_request = requests.get(HELFI_HOST+path)
-    proxy_request = requests.get(PROXY_HOST+path)
-    
-    if helfi_request != proxy_request:
-        text = f"""{'-'*40} \nRedirection mismatch \nSOURCE = {path}\nHelfi redirected to = {helfi_request.url}\nproxy redirected to = {proxy_request.url}\n{'-'*40}\n"""
-        print(text)
+
+if __name__ == "__main__":
+    main()
